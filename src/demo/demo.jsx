e353ab3088681948f8ff39f4158e1ffd085005e4
lxj <1073830130@qq.com>
2020-02-19 02:42:41 +0800

    feta: 一些优化和更新
    1. request 的 extraOptions支持大部分createRequest的配置，用于进行局部覆盖配置
    2. 整体流程优化
    3. 增加了接收非[error, response]的常规Promise
    4. 定义文件更新

--- src/demo/demo.jsx
+++ src/demo/demo.jsx
@@ -3,52 +3,31 @@ import React from 'react';
 import createRequest from '../source/request';
 
 let request = createRequest({
-  /* axios相关 */
-  // 创建axios实例时传入的初始配置
   axiosBaseConfig: {
     baseURL: '',
     timeout: 3000,
   },
-
-  /* 服务端约定配置 */
   serverMsgField: 'message', // 服务端返回的消息提示所在的属性名
   checkStatus(data) {
     // 接收服务端返回并根据约定的属性名返回一个bool值帮助判定本次请求成功与否
-    return data.status === true;
+    return data.code === 0;
   },
-
-  /* 自定义错误反馈方式 */
-  /**
-   * @description 用于自定义信息反馈的插件, 不用处理quite的情况
-   * @message {string} 根据response生成的一段提示文本
-   * @status {boolean} 状态 true:成功 false:失败。 根据response自动生成
-   * @extraOption {object} 请求时传入的额外配置
-   */
   feedBack(message, status, extraOption) {
     console.log(status ? '成功:' : '失败:', message, extraOption);
   },
-  /* 默认会原样返回response，可以通过此方法对返回格式化 */
   formatResponse(res) {
-    return res.data;
+    return res;
   },
-
-  /**
-   * 开始/结束请求，用于配置加载状态和往header上挂公共token等
-   * option是请求是传入的配置对象
-   * requestConfig 是传递给axios的option配置项, 用于在发送之前手动增加或修改某些配置
-   *  */
   startRequest(extraOption, requestConfig) {
-    console.log('----请求开始----', requestConfig);
+    // console.log('----请求开始----', requestConfig);
     // return 'flag';
   },
-
-  /* flag是startRequest中return的值，一般用于关闭loading时对弹窗组件进行标记 */
   finishRequest(extraOption, flag) {
-    console.log('----请求结束----', extraOption);
+    // console.log('----请求结束----', extraOption);
   },
 });
 
-request('http://127.0.0.1:4567/api/get?name=lxj&age=18', {
+request('http://127.0.0.1:3000/user', {
   data: {
     name: 'lx11j',
     age: 18,
@@ -57,11 +36,17 @@ request('http://127.0.0.1:4567/api/get?name=lxj&age=18', {
     // expirys: 3,
     useServeMsg: true,
     quiet: false,
-    plain: true,
+    // plain: true,
+    formatResponse(res) {
+      return res.data;
+    },
+    feedBack(message, status, extraOption) {
+      console.log(111, message)
+    }
   },
 })
   .then(res => {
-    console.log(1, res);
+    console.log(res);
   });
 
 const Demo = () => {
--- src/source/request.js
+++ src/source/request.js
@@ -1,29 +1,28 @@
-/**
- * 调用axios发起请求，返回[err, res]形式的结果, 根据传入配置可进行缓存、配置化的错误反馈、显示loading、 挂token等。
- *
- * @url  {string}   请求地址
- * @option?  {object}  传递给axios的option对象,下面的是除文档参数外新增的几个额外参数
- *  @extraOption
- *    @expirys?  {boolean|number}  设置缓存时间，值为true时缓存30s，为数字时表示指定的秒数。
- *    @useServeMsg?  {boolean}  为true时即使返回状态码正确依然会以服务器返回的消息(根据serverMsgField配置项确定)作为反馈提示。
- *    @quiet?  {boolean}  静默模式，无论正确与否不会有任何提示
- *    @plain?  {boolean}  用原始的response代替经过format函数的response
- * @return {array} 一个错误优先的数组 => [err, res]
- *
- * #额外实例属性#
- * #axios  获取axios实例, 当需要直接操作axios接口时使用
- *
- * 依赖包:
- * axios、lodash/get
- *
- * * 当服务端返回裸数据时，可以任意为serverMsgField设置一个字符值，此时根据状态码进行的错误反馈依然是可用的, 如果遇到连状态码码都乱给的后端的话...emmm
- */
-
 import axios from 'axios';
 import statusCode from './statusCode';
 import hashFnv32a from './hash';
 
-function createRequest(config = {}) {
+function getGlobal() {
+  return typeof window !== 'undefined' ? window : global;
+}
+
+const dumpFn = () => {};
+
+const defaultConf = {
+  axiosBaseConfig: {},
+  formatResponse: (response) => response,
+  startRequest: dumpFn,
+  finishRequest: dumpFn,
+  feedBack: dumpFn,
+};
+
+function createRequest(_config = {}) {
+  const config = {
+    ...defaultConf,
+    ..._config,
+  };
+
+  /* axios预设 */
   const axiosInstance = axios.create({
     ...config.axiosBaseConfig,
     headers: {
@@ -33,18 +32,31 @@ function createRequest(config = {}) {
 
   function request(url, reqOption = {}) {
     const option = reqOption.extraOption || {};
-    const quiet = !!option.quiet;
 
-    /* 缓存处理 (根据配置选项，为数字时取该数字的秒数，为truty时默认30秒) */
+    // 一些配置提前提出来
+    const quiet = !!option.quiet;
+    const formatResponse = option.formatResponse || config.formatResponse;
+    const feedBack = option.feedBack || config.feedBack;
+    const startRequest = option.startRequest || config.startRequest;
+    const finishRequest = option.finishRequest || config.finishRequest;
+    const checkStatus = option.checkStatus || config.checkStatus;
+    const serverMsgField = option.serverMsgField || config.serverMsgField;
+
+    const ptGlobal = getGlobal();
+
+    /* ------- 缓存处理 (根据配置选项，为数字时取该数字的秒数，为truty时默认30秒) ------- */
     const expirys =
       option.expirys &&
-      (typeof option.expirys === 'number' ? option.expirys : 30);
-    let fingerprint;
-    let hashcode;
+      (typeof option.expirys === 'number'
+        ? option.expirys
+        : 30
+      );
+    let fingerprint; // 根据当前请求参数、地址生成唯一串作为令牌
+    let hashcode;  // 根据fingerprint生成hash
 
     if (expirys) {
-      /* 根据url + params + data 生成hash，用于缓存请求结果 */
-      /* 包含函数、formdata等特殊类型的data不能进行缓存 (* 缓存只应该用于查询类的接口) */
+      /* 根据url + params + data (针对不使用params的后端) 生成hash，用于缓存请求结果 */
+      /* 包含函数、formData等特殊类型的data不能进行缓存 (* 缓存只应该用于查询类的接口) */
       fingerprint =
         url +
         JSON.stringify(reqOption.data || {}) +
@@ -52,22 +64,27 @@ function createRequest(config = {}) {
 
       hashcode = hashFnv32a(fingerprint);
 
-      // 尝试取出匹配到的缓存数据
-      let cached = sessionStorage.getItem(hashcode);
-      let cachedTime = sessionStorage.getItem(`${hashcode}:timestamp`);
+      /* 尝试取出匹配到的缓存数据 */
+      let cached = ptGlobal.sessionStorage.getItem(hashcode);
+      let cachedTime = ptGlobal.sessionStorage.getItem(`${hashcode}:timestamp`);
 
       if (cached && cachedTime) {
+        // 当前时间到缓存结束时间差值
         const age = (Date.now() - cachedTime) / 1000;
+
+        // 缓存生效，添加isCache标记后原样返回
         if (age < expirys) {
-          // 缓存生效，添加isCache标记后原样返回
           let _cached = JSON.parse(cached);
-          _cached.data.isCache = true;
-          return Promise.resolve([
-            null,
-            option.plain
-              ? checkResponse(_cached)
-              : config.formatResponse(checkResponse(_cached))
-          ]);
+
+          if (_cached) {
+            _cached.data.isCache = true;
+            return Promise.resolve([
+              null,
+              option.plain
+                ? checkResponse(_cached)
+                : formatResponse(checkResponse(_cached), option, reqOption) /* TODO: 添加自定义缓存, 传递的配置 */
+            ]);
+          }
         }
         // 缓存过期、清空
         sessionStorage.removeItem(hashcode);
@@ -85,23 +102,26 @@ function createRequest(config = {}) {
       ...reqOption
     };
 
-    let reqFlag = config.startRequest(option, requestConfig);
+    // 接收startRequest返回值传递给finishRequest, 用于结束loading等
+    let reqFlag = startRequest(option, reqOption);
 
     // 发起请求并进行一系列处理
     return axiosInstance(requestConfig)
       .then(checkResponse)
       .then(res => (expirys ? cache(res, hashcode) : res))
-      .then(res => [null, option.plain ? res : config.formatResponse(res)]) // 根据format配置处理数据
+      // 到这一步已经成功了
+      .then(res => [null, option.plain ? res : formatResponse(res, option, reqOption)]) // 根据format配置处理数据
       .catch(errorHandle)
-      .finally(() => config.finishRequest(option, reqFlag));
+      .finally(() => finishRequest(option, reqFlag));
 
     /* 接收response，处理数据，根据配置进行某些操作 */
     function checkResponse(response) {
       if (!quiet) {
-        const message = response.data && response.data[config.serverMsgField];
+        // 根据配置取出对应的message字段
+        const message = response.data && response.data[serverMsgField];
 
         // 如果后端约定的返回值有异常则抛出错误
-        if (!config.checkStatus(response.data)) {
+        if (!checkStatus(response.data)) {
           const error = new Error(message || 'server returned error');
           error.response = response;
           throw error;
@@ -109,7 +129,7 @@ function createRequest(config = {}) {
 
         // 返回正常但配置了useServeMsg且返回中包含message
         if (option.useServeMsg) {
-          message && config.feedBack(message, true, option);
+          message && feedBack(message, true, option, reqOption);
         }
       }
 
@@ -118,7 +138,7 @@ function createRequest(config = {}) {
     }
 
     /* 错误处理 */
-    function errorHandle(error) {
+    function errorHandle(error = {}) {
       // 处理axios相关的错误码
       // if (error.code && error.isAxiosError) {
       //   if (error.code === 'ECONNABORTED') {
@@ -136,12 +156,12 @@ function createRequest(config = {}) {
         message = `${response.status}: ${message}`;
 
         // 服务器状态码异常且包含serverMsgField
-        const serverMessage = response.data && response.data[config.serverMsgField];
+        const serverMessage = response.data && response.data[serverMsgField];
 
-        !quiet && config.feedBack(serverMessage || message, false, option);
+        !quiet && feedBack(serverMessage || message, false, option);
       } else {
         /* 没有状态码、没有服务器返回、也不在捕获范围内(跨域、地址出错完全没有发送到服务器时会出现) */
-        !quiet && config.feedBack(error.message || 'unknown error', false, option);
+        !quiet && feedBack(error.message || 'unknown error', false, option);
       }
 
       return [error, null];
@@ -151,12 +171,25 @@ function createRequest(config = {}) {
     function cache(res, hashcode) {
       const contentType = res.headers['content-type'];
       if (contentType && contentType.match(/application\/json/i)) {
-        sessionStorage.setItem(hashcode, JSON.stringify(res));
-        sessionStorage.setItem(`${hashcode}:timestamp`, Date.now());
+        ptGlobal.sessionStorage.setItem(hashcode, JSON.stringify(res));
+        ptGlobal.sessionStorage.setItem(`${hashcode}:timestamp`, String(Date.now()));
       }
       return res;
     }
   }
+
+  request.common = (...args) => {
+    return new Promise((resolve, reject) => {
+      request(...args)
+        .then(([err, res]) => {
+          if (err) {
+            reject(err);
+          }
+          resolve(res);
+        })
+        .catch((err) => reject(err));
+    });
+  };
 
   /* 暴露axios实例、返回request方法 */
   request.axios = axiosInstance;
